0738d9b3a8fdafef97d66c3f9a34d3dce425da2a
Hri <hrijan04@gmail.com>
2023-01-21 16:11:05 +0545

    completed details pages

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -6,9 +6,17 @@ export default function Details() {
     let {id} = useParams()
     let display = TrekPages.find((a) => a.id == id)
   return (
-    <div>
-      <h1>{display.name}</h1>
+    <div className='details'>
+      <div className="image-part">
+        <img src={display.image} alt="" />
+        <div className="overlay"></div>
+        <h1>{display.name}</h1>
+      </div>
       <p>{display.desc}</p>
+
+      <div className="book-now">
+        <button type="button" class="btn btn-outline-success">Book This Trek Now!</button>
+      </div>
     </div>
   )
 }
