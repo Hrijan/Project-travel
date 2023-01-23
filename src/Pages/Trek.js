975fddf3d280ea61bc0a1eac69c5b47a0e192fe9
Hri <hrijan04@gmail.com>
2023-01-23 19:19:08 +0545

    aos to trek page

--- src/Pages/Trek.js
+++ src/Pages/Trek.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import TrekPages from '../TrekPages';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 export default function Trek() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <div className='trek'>
       <h1>The Treks We Offer</h1>
       <ul>
         {TrekPages.map((a) => (
-            <Link to={`/details/${a.id}`}><li><div className="overlay"></div><img src={a.image} alt="" /><span>{a.name}</span></li></Link>
+            <Link to={`/details/${a.id}`}><div data-aos= "fade-right"><li><div className="overlay"></div><img src={a.image} alt="" /><span>{a.name}</span></li></div></Link>
         ))}
       </ul>
     </div>
